f1b2c8e17e80882e36e1e9fc04d290704ecce411
Liinhleo <17520691@gm.uit.edu.vn>
2020-04-19 23:29:46 +0700

    Divide into grid and reponsive

--- src/App.js
+++ src/App.js
@@ -2,47 +2,27 @@ import React from 'react';
 import './App.css';
 import NewsItem from './components/NewsItem.js';
 import data from './dataset-fake.json';
-import { Row, Col } from 'antd';
+
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col span={6}>
-            <div>col-6</div>
-          </Col>
-          <Col  span={6}>
-            <div>col-6</div>
-          </Col>
-          <Col span={6}>
-            <div>col-6</div>
-          </Col>
-          <Col span={6}>
-            <div>col-6</div>
-          </Col>
-        </Row>
-        {/* {
+        {
           data.map((item, index) => {
             return (
-              <Row>
-                <Col span={12}>
-                  <NewsItem
-                    key={index}
-                    tag={item.tag}
-                    title={item.title}
-                    author={item.author}
-                    content={item.content} />
-                </Col>
-                <Col span={12}>
-                  <NewsItem
-                    key={index}
-                    image={item.image} />
-                </Col>
-              </Row>
+              <NewsItem
+                key={index}
+                tag={item.tag}
+                title={item.title}
+                author={item.author}
+                timer={item.timer}
+                image={item.image}
+                content={item.content} />
             )
           })
-        } */}
+        }
       </div>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import './NewsItem.css';
-
+import { Row, Col } from 'antd';
 
 class NewsItem extends React.Component {
   render() {
-    const p_tag = {
-      color: "green",
-      fontSize: "13px"
-    };
-
-    const p_author = {
-      color: "grey",
-      fontSize: "13px"
-    };
-    const p_content = {
-      fontSize: "18px"
-    };
-
     return (
       <div className="NewsItem">
-        <p style={p_tag}>{this.props.tag}</p>
-        <h1>{this.props.title}</h1>
-        <p style={p_author}>{this.props.author}</p>
-        <p style={p_content}>{this.props.content}</p>
-        <img
-          src={this.props.image}
-          width="250" />
+        <Row gutter={10}>
+          <Col span={18} xs={16}>
+            <p className="p_tag">{this.props.tag}</p>
+            <h1 className="p_title">{this.props.title}</h1>
+
+            <Row gutter={10}> 
+              <Col span={4} xs={8} lg={4}>
+                <p className="p_author">{this.props.author}</p>
+              </Col>
+              <Col span={20} xs={16} lg={20}>             
+                <p className="p_timer">{this.props.timer}</p>
+              </Col>
+            </Row>
+
+            <span className="sp_content">{this.props.content}</span>
+          </Col>
+          <Col span={6} xs={8}>
+            <img
+              className="thump"
+              src={this.props.image} />
+          </Col>
+        </Row>
       </div>
     );
   }
